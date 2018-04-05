004513611eba13e388fc2ef3db4766ecedb16ec4
yumras <yumras@naver.com>
2018-04-05 15:44:52 +0900

    Example_Join

--- src/main/scala/com/haiteam/Example_Join.scala
+++ src/main/scala/com/haiteam/Example_Join.scala
@@ -15,18 +15,49 @@ object Example_Join {
   var subData = spark.read.format("csv").
     option("header", "true").load(dataPath + subFile)
 
-  mainData.createTempView("maindata")
-  subData.createTempView("subdata")
+  mainData.createOrReplaceTempView("maindata")
+  subData.createOrReplaceTempView("subdata")
 
   var leftJoinData = spark.sql("select a.*, b.productname " +
-    "from maindata a left outer join subdata b " +
+    "from maindata a left join subdata b " +
     "on a.productgroup = b.productid")
 
   var innerData = spark.sql("select a.*, b.productname " +
     "from maindata a inner join subdata b " +
     "on a.productgroup = b.productid")
 
-  leftJoinData.createTempView("leftJoinData")
-  innerData.createTempView("innerData")
+  leftJoinData.createOrReplaceTempView("leftJoinData")
+  innerData.createOrReplaceTempView("innerData")
+  leftJoinData.show
+  innerData.show
+
+
+  var staticUrl = "jdbc:oracle:thin:@192.168.110.111:1521/orcl"
+  var staticUser = "kopo"
+  var staticPw = "kopo"
+  var mainFile1 = "kopo_channel_seasonality_new"
+  var subFile1 = "kopo_region_mst"
+
+  val mainData1= spark.read.format("jdbc").options(Map("url" -> staticUrl,"dbtable" -> mainFile1,"user" -> staticUser, "password" -> staticPw)).load
+  val subData1= spark.read.format("jdbc").options(Map("url" -> staticUrl,"dbtable" -> subFile1,"user" -> staticUser, "password" -> staticPw)).load
+
+  mainData.createOrReplaceTempView("maindata1")
+  subData.createOrReplaceTempView("subdata1")
+
+
+  var leftJoinData1 = spark.sql("select a.regionid, b.regionname, a.product, a.yearweek, a.qty " +
+    "from maindata1 a left join subdata1 b " +
+    "on a.regionid = b.regionid")
+
+  var innerData1 = spark.sql("select a.regionid, b.regionname, a.product, a.yearweek, a.qty " +
+    "from maindata1 a inner join subdata1 b " +
+    "on a.regionid = b.regionid")
+
+  leftJoinData1.createOrReplaceTempView("leftJoinData")
+  leftJoinData1.show
+
+  innerData1.createOrReplaceTempView("innerData")
+  innerData1.show
+
 
 }
