80c28afe4a9cb16b6feff342b4bdfdb481f0b0e2
yumras <yumras@naver.com>
2018-04-13 10:45:03 +0900

    orcl data join 0412 df to rdd

--- src/main/scala/com/haiteam/OrclDataJoin1_0412.scala
+++ src/main/scala/com/haiteam/OrclDataJoin1_0412.scala
@@ -7,19 +7,19 @@ object OrclDataJoin1_0412 {
     val spark = SparkSession.builder().config("spark.master","local").getOrCreate()
 
     // oracle connection
-    var staticUrl = "jdbc:oracle:thin:@192.168.110.111:1521/orcl"
+    val staticUrl = "jdbc:oracle:thin:@192.168.110.111:1521/orcl"
 
-    var staticUser = "kpo"
+    var staticUser = "kopo"
     var staticPw = "kopo"
     var selloutDb = "kopo_channel_seasonality_new"
     var productNameDb = "kopo_product_mst"
 
-    val selloutDf = spark.read.format("jdbc").
+    var selloutDf = spark.read.format("jdbc").
       options(Map("url" -> staticUrl, "dbtable" -> selloutDb,
         "user" -> staticUser,
         "password" -> staticPw)).load
 
-    val productMasterDf = spark.read.format("jdbc").
+    var productMasterDf = spark.read.format("jdbc").
       options(Map("url" -> staticUrl, "dbtable" -> productNameDb,
         "user" -> staticUser,
         "password" -> staticPw)).load
@@ -33,10 +33,11 @@ object OrclDataJoin1_0412 {
       "a.product, " +
       "a.yearweek, " +
       "cast(a.qty as double) as qty, " +
-      "b.product_name " +
+      "b.productname " +
       "from selloutTable a " +
       "left join mstTable b " +
-      "on a.product = b.product_id")
+      "on a.product = b.productid")
+
 
     rawData.show(2)
 
@@ -49,6 +50,16 @@ object OrclDataJoin1_0412 {
     var productnameNo = rawDataColumns.indexOf("productname")
 
     var rawRdd = rawData.rdd
+
+    // 설정 부적합 로직 정제
+    var rawExRdd = rawRdd.filter(x=>{
+      var checkValid = true
+      // 설정 부적합 로직
+      if (x.getString(yearweekNo).length !=6){
+        checkValid = false;
+      }
+      checkValid
+    })
 
   }
 }
